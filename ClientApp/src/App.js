125b73685e2b116aa79f43478bf9a3682707b17a
lauratumaite <lauratumaite0@gmail.com>
2020-11-29 17:09:09 +0200

    budget manager without db

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -9,6 +9,13 @@ import FetchExpensesManagerInfo from "./components/ExpensenesComponents/FetchExp
 import AddLimit from "./components/ExpensenesComponents/AddLimit";
 import EditExpensesInfo from "./components/ExpensenesComponents/EditExpensesInfo";
 import './custom.css'
+import { Header } from './components/BudgetManagerComponents/Header';
+import { Balance } from './components/BudgetManagerComponents/Balance';
+import { IncomeExpenses } from './components/BudgetManagerComponents/IncomeExpenses';
+import { TransactionList } from './components/BudgetManagerComponents/TransactionList';
+import { AddTransaction } from './components/BudgetManagerComponents/AddTransaction';
+import { GlobalProvider } from './components/BMcontextAPI/GlobalState'
+import './BMapp.css'
 
 export default class App extends Component {
   static displayName = App.name;
@@ -20,12 +27,22 @@ export default class App extends Component {
         <Route path='/SavingsManagerInformations' exact component={FetchSavingInfo} />
         <Route path="/SavingsManagerInformations/:id" component={SavingInfoDetails} />
         <Route path='/ExpensesManagerInformations' exact component={FetchExpensesManagerInfo} />
-         <Route path="/ExpensesManagerInformations/add" excat component={AddLimit} />
-         <Route path="/ExpensesManagerInformations/edit/:id" excat component={EditExpensesInfo} />
-            <Route exact path='/' exact component={Home} />
-            <Route path='/SavingsManagerInformations' exact component={SavingInfoApp} />
-            <Route path="/SavingsManagerInformations/:id" exact component={SavingInfoDetails} />
+        <Route path="/ExpensesManagerInformations/add" excat component={AddLimit} />
+        <Route path="/ExpensesManagerInformations/edit/:id" excat component={EditExpensesInfo} />
+        <Route path='/SavingsManagerInformations' exact component={SavingInfoApp} />
+        <Route path="/SavingsManagerInformations/:id" exact component={SavingInfoDetails} />
+
+        <GlobalProvider>
+        <Header />
+            <div className="container">
+                <Balance />
+                <IncomeExpenses />
+                <TransactionList />
+                <AddTransaction />
+            </div>
+        </GlobalProvider>
       </Layout>
     );
   }
 }
+
--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -11,7 +11,7 @@ const rootElement = document.getElementById('root');
 
 ReactDOM.render(
   <BrowserRouter basename={baseUrl}>
-    <App />
+        <App />
   </BrowserRouter>,
   rootElement);
 
