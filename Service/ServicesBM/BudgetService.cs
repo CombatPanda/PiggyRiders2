c9f3d123732d1713ee108ab98dc85b6e5201a43d
ddovile <71456312+ddovile@users.noreply.github.com>
2021-01-03 00:44:42 +0200

    pridet userID

--- Service/ServicesBM/BudgetService.cs
+++ Service/ServicesBM/BudgetService.cs
@@ -11,24 +11,37 @@ namespace SmartSaver.Service.ServicesBM
     public class BudgetService : IBudgetService
     {
         private readonly UserContext context;
-        public BudgetService(UserContext context)
+        private readonly IJWTService service;
+        public BudgetService(UserContext context, IJWTService service)
         {
             this.context = context;
+            this.service = service;
         }
         public async Task Add(UserBudget budget)
         {
+           int user = UserID();
+           budget.userID = user;
            context.UserBudget.Add(budget);
            await context.SaveChangesAsync();
         }
 
         public async Task<List<UserBudget>> GetAll()
         {
+            int user = UserID();
             List<UserBudget> budget = new List<UserBudget>();
             using (context)
             {
-                budget = await context.UserBudget.ToListAsync();
+                budget = await context.UserBudget.Where(b=>b.userID==user).ToListAsync();
             }
             return budget;
+        }
+        private int UserID()
+        {
+            int userID;
+            bool convertable = Int32.TryParse(service.GetID(), out userID);
+            if (convertable)
+                return userID;
+            else throw new Exception("Invalid user id");
         }
     }
 }
